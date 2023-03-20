5bd461797f41c1e3f8fa24a2b77841d68197a994
LukasMitlener <97617930+LukasMitlener@users.noreply.github.com>
2023-03-20 14:59:33 +0100

    Add files via upload

--- styles/index.js
+++ styles/index.js
@@ -15,7 +15,7 @@ const styles = {
 
   // hero section
   heroHeading:
-      'font-bold font-AmaticSC lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-[#8D5CC7]',
+      'font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-[#8D5CC7]',
 };
 
 export default styles;
